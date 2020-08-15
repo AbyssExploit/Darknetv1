7af668ce17f9221de44017b833d2b668d3414e52
AbyssWalker <63506758+AbyssExploit@users.noreply.github.com>
2020-08-16 04:49:12 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/system/xbin/bash
+
+apt install bash -y
+apt install python -y
+apt install figlet -y
+apt install ruby -y
+gem install lolcat -y
+apt install toilet -y
+apt install python2 -y
+apt install cowsay -y
+pip2 install requests
+pip2 install mechanize
+pip install requests
+pip install mechanize
